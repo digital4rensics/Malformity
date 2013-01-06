cd27b44811764d1c41fb5a27c14f57859276c933
Kyle Maxwell <kylem@xwell.org>
2013-01-06 16:17:05 -0600

    Added Virustotal transforms

---
+++ src/Malformity/transforms/vt_hash2name.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+from canari.maltego.utils import debug, progress
+from canari.framework import configure #, superuser
+from canari.config import config
+from common.entities import Hash
+from canari.maltego.entities import Phrase
+from canari.maltego.message import Label, UIMessage
+from urllib import urlopen, urlencode
+from json import loads
+
+
+__author__ = 'Kyle Maxwell'
+__copyright__ = 'Copyright 2012, Kyle Maxwell'
+__credits__ = []
+
+__license__ = 'GPL'
+__version__ = '0.1'
+__maintainer__ = 'Kyle Maxwell'
+__email__ = '@kylemaxwell'
+__status__ = 'Development'
+
+__all__ = [
+    'dotransform'
+]
+
+
+@configure(
+    label="To malware name [VT]",
+    description="Returns names of malware associated with a particular hash",
+    uuids=['malformity.v1.VT_Hash2Name'],
+    inputs=[('VirusTotal', Hash)]
+)
+
+def dotransform(request, response):
+    debug('VT API key %s\n' % request.value)
+
+    r = urlopen(
+        "https://www.virustotal.com/vtapi/v2/file/report",
+        urlencode({
+            "resource": request.value,
+            "apikey": config['virustotal/apikey']
+        })
+    )
+
+    if r.code == 200:
+        d = loads(r.read())
+        debug('VT output: %s\n' % d)
+
+        # If it's not a clean file, tell Maltego the names of the malware
+        if d['response_code'] == 1:
+            for engine in d['scans'].iteritems():
+                if engine[1]['detected']:
+                    e = Phrase(engine[1]['result'])
+                    e += Label("VirusTotal Report", d['permalink'])
+                    response += e
+
+        response += UIMessage(d['verbose_msg'])
+
+    return response
+
---
+++ src/Malformity/transforms/vt_url2engine.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+from canari.maltego.utils import debug, progress
+from canari.framework import configure #, superuser
+from canari.config import config
+from canari.framework import configure
+from canari.maltego.message import Label
+from canari.maltego.utils import debug
+from canari.maltego.entities import Phrase, URL
+from common.entities import Hash
+from json import loads
+from urllib import urlopen, urlencode
+
+__author__ = 'Kyle Maxwell'
+__copyright__ = 'Copyright 2012, Kyle Maxwell'
+__credits__ = []
+
+__license__ = 'GPL'
+__version__ = '0.1'
+__maintainer__ = 'Kyle Maxwell'
+__email__ = '@kylemaxwell'
+__status__ = 'Development'
+
+__all__ = [
+    'dotransform',
+]
+
+
+@configure(
+    label="To AV Engine [VT]",
+    description="Returns names of AV engines that flagged the URL",
+    uuids=['malformity.v1.VT_URL2Engine'],
+    inputs=[('VirusTotal', URL)]
+)
+
+def dotransform(request, response):
+
+    debug('VT API key %s\n' % config['virustotal/apikey'])
+
+    r = urlopen(
+        "https://www.virustotal.com/vtapi/v2/url/report",
+        urlencode({
+            "resource": request.value,
+            "apikey": config['virustotal/apikey']
+        })
+    )
+
+    if r.code == 200:
+        d = loads(r.read())
+        debug('VT output: %s\n' % d)
+
+        if d['response_code'] == 1:
+            for engine in d['scans'].iteritems():
+                if engine[1]['detected']:
+                    e = Phrase(engine[0])
+                    e += Label("VirusTotal Report", d['permalink'])
+                    response += e
+
+    return response
